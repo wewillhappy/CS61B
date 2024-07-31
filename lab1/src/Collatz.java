45e941f97dbdd746b3c29eff9ffbb2f32d246782
T <24170@qq.com>
2024-07-31 19:12:01 +0800

    first commit

--- lab1/src/Collatz.java
+++ lab1/src/Collatz.java
@@ -9,6 +9,15 @@ public class Collatz {
              num=nextNumber(num);
          }
      }
+    public class ArrayExample {
+        public static void main(String[] args) {
+            int [] input = new int[5];
+            for(int i = 0; i < 5; i++){
+                input[i] = i + 1;
+            }
+        }
+    }
+
      public static int nextNumber(int n) {
          if(n %2==0){
              return n/2;
